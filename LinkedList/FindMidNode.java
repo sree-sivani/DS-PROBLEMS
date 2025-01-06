f93e175001a8c718c5bb6dfdabf62249432baf6c
Sree Sivani <111432178+sree-sivani@users.noreply.github.com>
2025-01-06 15:28:37 +0530

    Create FindMidNode.java

---
+++ LinkedList/FindMidNode.java
@@ -0,0 +1,22 @@
+/* Node of a linked list
+ class Node {
+   int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+*/
+
+class FindMidNode {
+    int getMiddle(Node head) {
+        // Your code here.
+        Node slow = head;
+        Node fast = head;
+        
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        
+        return slow.data;
+    }
+}
